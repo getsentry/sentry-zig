41ee15cfd8cad02c6580e30ca9bbd245eb660928
Alexander Alderman Webb <alexander.webb@sentry.io>
2025-08-18 15:45:15 +0200

    fix envelope header type

--- src/types/SentryEnvelopeHeader.zig
+++ src/types/SentryEnvelopeHeader.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
+const UUID = @import("Types.zig").UUID;
+
 pub const SentryEnvelopeHeader = struct {
-    sentry_id: SentryItemType,
+    sentry_id: UUID,
     sdk_version: i64,
     trace_context: ?[]const u8 = null,
     sent_at: ?[]const u8 = null,
