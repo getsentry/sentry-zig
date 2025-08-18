6e3729dcc2698c20f078d819a81470cd534de220
Simon Roth <39389607+srothh@users.noreply.github.com>
2025-08-18 14:59:34 +0200

    feat(types): add Dsn (#8)
    Co-authored-by: Giannis Gkiortzis <giannis.gkiortzis@sentry.io>
    Co-authored-by: Alexander Alderman Webb <alexander.webb@sentry.io>
    Co-authored-by: lcian <lorenzo.cian@sentry.io>

--- src/Types.zig
+++ src/Types.zig
@@ -3,3 +3,4 @@ pub const BreadcrumbType = @import("types/BreadcrumbType.zig").BreadcrumbType;
 pub const Level = @import("types/Level.zig").Level;
 pub const User = @import("types/User.zig").User;
 pub const Breadcrumb = @import("types/Breadcrumb.zig").Breadcrumb;
+pub const Dsn = @import("types/Dsn.zig").Dsn;
