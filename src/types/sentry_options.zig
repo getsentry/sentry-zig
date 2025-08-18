4aae0c23d726336af87c821adcc464fdb7c0d731
lcian <lorenzo.cian@sentry.io>
2025-08-18 14:10:08 +0200

    deinit functions

--- src/types/sentry_options.zig
+++ src/types/sentry_options.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
 pub const SentryOptions = struct {
     dsn: ?[]const u8 = null,
     environment: ?[]const u8 = null,
@@ -5,4 +8,10 @@ pub const SentryOptions = struct {
     debug: bool = false,
     sample_rate: f64 = 1.0,
     send_default_pii: bool = false,
+
+    pub fn deinit(self: *SentryOptions, allocator: Allocator) void {
+        if (self.dsn) |dsn| allocator.free(dsn);
+        if (self.environment) |environment| allocator.free(environment);
+        if (self.release) |release| allocator.free(release);
+    }
 };
