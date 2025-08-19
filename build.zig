c23d09794e47165f2b77662d03b3e4de0ce52a67
lcian <lorenzo.cian@sentry.io>
2025-08-19 14:27:31 +0200

    we good

--- build.zig
+++ build.zig
@@ -73,18 +73,18 @@ pub fn build(b: *std.Build) void {
     // Create a run step that can be invoked with "zig build run"
     const run_step = b.step("run", "Run the demo application");
     run_step.dependOn(&run_exe.step);
-    const exe = b.addExecutable(.{
+    const exe2 = b.addExecutable(.{
         .name = "send_empty_envelope",
         .root_source_file = b.path("src/send_empty_envelope.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.linkLibrary(lib);
-    exe.root_module.addImport("types", types);
+    exe2.linkLibrary(lib);
+    exe2.root_module.addImport("types", types);
 
-    b.installArtifact(exe);
+    b.installArtifact(exe2);
 
-    const run_cmd = b.addRunArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe2);
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
