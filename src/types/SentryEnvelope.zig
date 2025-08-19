46407d8f955533bdb7dad91031c3600b45918c44
Giannis Gkiortzis <giannis.gkiortzis@sentry.io>
2025-08-19 10:46:24 +0200

    fix import path

--- src/types/SentryEnvelope.zig
+++ src/types/SentryEnvelope.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-const SentryEnvelopeHeader = @import("Types.zig").SentryEnvelopeHeader;
-const SentryEnvelopeItem = @import("Types.zig").SentryEnvelopeItem;
+const SentryEnvelopeHeader = @import("../Types.zig").SentryEnvelopeHeader;
+const SentryEnvelopeItem = @import("../Types.zig").SentryEnvelopeItem;
 
 pub const SentryEnvelope = struct {
     header: SentryEnvelope,
