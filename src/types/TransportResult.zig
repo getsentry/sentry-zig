3a593ecfec8600ed6d6c65d7304ff3eea17f1e58
Alexander Alderman Webb <alexander.webb@sentry.io>
2025-08-19 10:56:25 +0200

    fix name

--- src/types/TransportResult.zig
+++ src/types/TransportResult.zig
@@ -1,4 +1,4 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-pub const SentryEnvelope = struct { response_code: i64 };
+pub const TransportResult = struct { response_code: i64 };
