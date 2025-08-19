8a393469c5ea82836235857d6395a21cb124931f
Alexander Alderman Webb <alexander.webb@sentry.io>
2025-08-19 10:47:59 +0200

    update imports

--- src/types/SentryEnvelope.zig
+++ src/types/SentryEnvelope.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-const SentryEnvelopeHeader = @import("Types.zig").SentryEnvelopeHeader;
-const SentryEnvelopeItem = @import("Types.zig").SentryEnvelopeItem;
+const SentryEnvelopeHeader = @import("types").SentryEnvelopeHeader;
+const SentryEnvelopeItem = @import("types").SentryEnvelopeItem;
 
 pub const SentryEnvelope = struct {
     header: SentryEnvelope,
