ae1800ac5a31ccdf02034421db47739fb343ce39
lcian <lorenzo.cian@sentry.io>
2025-08-20 11:03:30 +0200

    test: refAllDeclsRecursive

--- src/root.zig
+++ src/root.zig
@@ -33,23 +33,10 @@ pub fn captureEvent(event: Event) ?EventId {
     return null;
 }
 
-pub fn captureError(err: anyerror) ?EventId {
-    const global_scope = scopes.getGlobalScope() catch return null;
-    const event = Event.fromError(global_scope.allocator, err);
-    defer event.deinit(global_scope.allocator);
-    return captureEvent(event);
-}
-
-pub fn captureMessage(
-    message: []const u8,
-    level: Level,
-) ?EventId {
-    const global_scope = scopes.getGlobalScope() catch return null;
-    const event = Event.fromMessage(global_scope.allocator, message, level);
-    defer event.deinit(global_scope.allocator);
-    return captureEvent(event);
-}
-
 test "test transport" {
     _ = @import("transport.zig");
+}
+
+test "compile check everything" {
+    std.testing.refAllDeclsRecursive(@This());
 }
